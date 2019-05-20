f6faf11afbc7505cb63fa4a74932be90a961a18d
Brian Schendt <brianschendt@gmail.com>
2019-05-20 09:49:42 -0400

    Two Sum

---
+++ TwoSum.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++)
+        {
+            map.put(nums[i],i);
+        }
+        for (int i = 0; i < nums.length; i++)
+        {
+            System.out.printf("%s,%s \n",nums[i],map.get(target-nums[i]));
+            if (map.containsKey(target-nums[i]) && i != map.get(target-nums[i])){
+                return new int[] {i, map.get(target-nums[i])};
+            }
+        }
+        
+        return null;
+    }
+
+    public static void main(String args[]){
+        //int[] nums = {2,7,11,15};
+        //int target = 9;
+        int[] nums = {3,2,4};
+        int target = 6;
+        
+        TwoSum solution = new TwoSum();
+        int[] result = solution.twoSum(nums, target);
+        System.out.printf("Expected: [0,1], got: %s",Arrays.toString(result));
+
+    }
+}
